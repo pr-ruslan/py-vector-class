5150add1670bed06866c17af50345f5612083264
Ruslan Priadko <r.priadko@ukr.net>
2024-12-31 12:02:13 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,29 +1,29 @@
 from typing import Any
-from math import sqrt
+import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, x_coord: float, y_coord: float) -> None:
+        self.x = round(x_coord, 2)
+        self.y = round(y_coord, 2)
 
     def __add__(self, other: Any) -> Any:
         return Vector(
-            x=self.x + other.x,
-            y=self.y + other.y
+            x_coord=self.x + other.x,
+            y_coord=self.y + other.y
         )
 
     def __sub__(self, other: Any) -> Any:
         return Vector(
-            x=self.x - other.x,
-            y=self.y - other.y
+            x_coord=self.x - other.x,
+            y_coord=self.y - other.y
         )
 
     def __mul__(self, other: Any) -> Any:
-        if isinstance(other, float):
+        if isinstance(other, (float, int)):
             return Vector(
-                x=self.x * other,
-                y=self.y * other
+                x_coord=self.x * other,
+                y_coord=self.y * other
             )
         elif isinstance(other, Vector):
             return self.x * other.x + self.y * other.y
@@ -34,18 +34,29 @@ class Vector:
             start_point: tuple,
             end_point: tuple) -> Any:
         return Vector(
-            x=end_point[0] - start_point[0],
-            y=end_point[1] - start_point[1]
+            x_coord=end_point[0] - start_point[0],
+            y_coord=end_point[1] - start_point[1]
         )
 
     def get_length(self) -> float:
-        return sqrt(self.x**2 + self.y**2)
+        return math.sqrt(self.x**2 + self.y**2)
 
-    def get_normalized(self, other: Any) -> Any:
-        pass
+    def get_normalized(self) -> Any:
+        return Vector(
+            x_coord=round(self.x / self.get_length(), 2),
+            y_coord=round(self.y / self.get_length(), 2)
+        )
 
-    def angle_between(self):
-        pass
+    def angle_between(self, other: Any) -> float:
+        angle = (self * other) / (self.get_length() * other.get_length())
+        return round(math.degrees(math.acos(angle)))
 
-    def get_angle(self):
-        pass
+    def get_angle(self) -> float:
+        return self.angle_between(Vector(0, 1))
+
+    def rotate(self, degrees: int) -> Any:
+        rotated_x = self.x * math.cos(math.radians(degrees)) \
+            - self.y * math.sin(math.radians(degrees))
+        rotated_y = self.x * math.sin(math.radians(degrees)) \
+            + self.y * math.cos(math.radians(degrees))
+        return Vector(x_coord=rotated_x, y_coord=rotated_y)
